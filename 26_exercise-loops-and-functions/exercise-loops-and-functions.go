c2d625afda80b5c2647c681a3f77f675e1c968a0
kogepanh <k.hosono1209@gmail.com>
2020-09-02 18:54:37 +0900

    edit

--- 26_exercise-loops-and-functions/exercise-loops-and-functions.go
+++ 26_exercise-loops-and-functions/exercise-loops-and-functions.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func Sqrt(x float64) float64 {
-	return x
+func Sqrt(x float64) (float64, int) {
+	z := 1.0
+	t := 1.0
+	c := 0
+	for {
+		c++
+		z -= (z*z - x) / (2 * z)
+		if math.Abs(z-t) < 0.000000000000001 {
+			return z, c
+		}
+		t = z
+	}
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	i := 1.0
+	for i <= 10 {
+		value, count := Sqrt(i)
+		fmt.Println(i, " -> ", value, ", ", count, "回")
+		i++
+	}
+	value, count := Sqrt(9)
+	fmt.Println("9 -> ", value, ", ", count, "回")
 }
