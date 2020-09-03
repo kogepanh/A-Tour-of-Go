ef97a2c02b4a925153b420db31e26bc807d67c5f
kogepanh <k.hosono1209@gmail.com>
2020-09-03 12:01:45 +0900

    edit

--- 55_exercise-maps/exercise-maps.go
+++ 55_exercise-maps/exercise-maps.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"strings"
+
 	"golang.org/x/tour/wc"
 )
 
 func WordCount(s string) map[string]int {
-	return map[string]int{"x": 1}
+	m := make(map[string]int)
+	for _, w := range strings.Fields(s) {
+		m[w]++
+	}
+	return m
 }
 
 func main() {
